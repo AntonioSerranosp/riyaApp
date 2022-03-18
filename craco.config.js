bea9b9900b8d2d8c96839428c428b835e016130a
Pablo Garay <pablogaray@Pablo.local>
2022-03-17 20:47:18 -0600

    added first palet colors in the config

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,11 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { "@primary-color": "#5AB2A7" },
+            modifyVars: { "@primary-color": "#8CBF55" },
+            javascriptEnabled: true,
+            modifyVars: { "@secondary-color": "#5AB2A7" },
+            javascriptEnabled: true,
+            modifyVars: { "@tertiaryy-color": "#57DBF74" },
             javascriptEnabled: true,
           },
         },
